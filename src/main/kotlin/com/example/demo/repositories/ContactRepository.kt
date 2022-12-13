a5b11f4ee30ea0e2a6e09589cee6204247c3d44d
Pedro Victor Saraiva de Sa - PrestServ <pedro.sa.prestserv@petrobras.com.br>
2022-12-13 10:56:58 -0300

    :feat: Criando Repository

---
+++ src/main/kotlin/com/example/demo/repositories/ContactRepository.kt
@@ -0,0 +1,13 @@
+package com.example.demo.repositories
+
+import com.example.demo.entities.ContactEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+/**
+ * Listagem ocorre pela Entity (Contact) e pela chave primária,
+ * no caso o Id do tipo Long.
+ * */
+@Repository
+interface ContactRepository: JpaRepository<ContactEntity, Long> {
+}
